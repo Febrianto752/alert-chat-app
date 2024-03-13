45afcb45158a6c128fec87c0a46e4ef75674c709
Febrianto752 <febrianto.bekasi@gmail.com>
2024-03-13 09:48:03 +0700

    feat: add apm agent

--- server/app.js
+++ server/app.js
@@ -1,3 +1,18 @@
+var apm = require('elastic-apm-node').start({
+    // Override service name from package.json
+    // Allowed characters: a-z, A-Z, 0-9, -, _, and space
+    serviceName: 'alert_chat_app',
+
+    // // Use if APM integration requires a token
+    // secretToken: '',
+
+    // // Use if APM integration uses API keys for authentication
+    // apiKey: '',
+
+    // Set custom APM integration host and port (default: http://127.0.0.1:8200)
+    serverUrl: 'http://127.0.0.1:8200',
+})
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
